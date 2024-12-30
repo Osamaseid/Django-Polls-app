66fad9f22eeeed569315a399f315a29d11c92477
osama seid <osaseid1@gmail.com>
2024-12-30 10:54:51 +0300

    polls app added to setting.py and configured

--- polls/models.py
+++ polls/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
-# Create your models here.
 
-class question(models.model):
-    question_text = models.CharField(max_length=200),
-    pub_date = models.DateTimeField('data published'),
-class choice(models.model):
-    question = models.ForeignKey(question, on_delete=models.CASCADE),
-    choice_text = models.CharField(max_length=200),
-    votes = models.IntegerField(default=0),
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
