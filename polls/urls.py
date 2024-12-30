ce41da4b6f95941317867581d35da0b51251950a
osama seid <osaseid1@gmail.com>
2024-12-30 15:33:37 +0300

    detail.html file updated

--- polls/urls.py
+++ polls/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     # ex: /polls/5/vote/
     path("<int:question_id>/vote/", views.vote, name="vote"),
     
-    path("<int:question_id>/", views.detail, name="detail"),path("<int:question_id>/", views.detail, name="detail"),
+    path("<int:question_id>/", views.detail, name="detail"),
+    path("<int:question_id>/", views.detail, name="detail"),
     path("specifics/<int:question_id>/", views.detail, name="detail"),
 ]
